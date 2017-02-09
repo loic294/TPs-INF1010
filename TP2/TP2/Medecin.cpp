c4681863e04840902ff60cff17e048f9fb5a369d
bergeronjulien <bergeron_julien@hotmail.com>
2017-02-08 21:33:51 -0500

    modif Medecin, specialite

--- TP2/TP2/Medecin.cpp
+++ TP2/TP2/Medecin.cpp
@@ -1,56 +1,98 @@
+/********************************************
+* Titre: Travail pratique #1 - Medecin.cpp
+* Date: 31 janvier 2017
+* Auteur: Julien Bergeron (1829496) et Loic Bellemare-Alford (1846135)
+*******************************************/
+
 #include "Medecin.h"
 #include <iostream>
 #include <iomanip>
 #include "Const.h"
 
+
+//Constructeur par paramètre
 Medecin::Medecin(const string& nom): nom_(nom), horaires_(0)
 {
 }
 
+
+/*
+Construteur avec les paramètres pour la classe Mdecin
+\param [in]		nom				Indique le nom
+\param [in]		horaires		Indique l'horaire
+\param [in]		uneSpecialiste	Indique un spécialité
+*/
 Medecin::Medecin(const string& nom, int horaires, Specialite* specialite): nom_(nom), horaires_(horaires), specialite_(specialite)
 {
 }
 
-Medecin::~Medecin() // A MODIFIER... (si necessaire)
+
+//destructeur
+Medecin::~Medecin() 
 {
-	
+	specialite_ = nullptr;
 }
 
+
+/*
+Méthode pour obtenir l'attribut
+\return nom_
+*/
 string Medecin::obtenirNom() const
 {
 	return nom_;
 }
 
+/*Méthode pour obtenir l'attribut
+\return horaire_
+*/
 int Medecin::obtenirHoraires() const
 {
 	return horaires_;
 }
 
+/*
+Méthode pour obtenir l'attribut
+\return specialite_
+*/
 Specialite Medecin::obtenirSpecialite() const
 {
 	return *specialite_;
 }
 
+/*
+Méthode pour modifier l'attribut
+\param[in]	nom		Indique le nom
+*/
 void Medecin::modifierNom(const string& nom)
 {
 	nom_ = nom;
 }
 
+/*
+Méthode pour obtenir l'attribut
+\return horaire_
+*/
 void Medecin::modifierHoraires(int horaires)
 {
 	horaires_ = horaires;
 }
 
+/*
+Méthode pour modifier l'attribut
+\param[in]	uneSpecialiste		Indique une specialite
+*/
 void Medecin::modifierSpecialite(Specialite* specialite)
 {
 	specialite_ = specialite;
 }
 
+//Affiche les informations de la classe Medecin
 void Medecin::information() const
 {
-	std::cout << "| " << medecin.nom_ << AFFICHER_ESPACE(espace_nom - medecin.nom_.size());
-	std::cout << " | " << AFFICHER_ESPACE(espace_horaires - std::to_string(medecin.horaires_).size()) << medecin.horaires_;
+	std::cout << "| " << nom_ << AFFICHER_ESPACE(espace_nom - nom_.size());
+	std::cout << " | " << AFFICHER_ESPACE(espace_horaires - std::to_string(horaires_).size()) << horaires_;
 	std::cout << AFFICHER_ESPACE(espace_horaires);
-	medecin.obtenirSpecialite().information();
+	obtenirSpecialite().information();
 	std::cout << endl;
 }
--- TP2/TP2/Specialite.cpp
+++ TP2/TP2/Specialite.cpp
@@ -1,40 +1,78 @@
+/********************************************
+* Titre: Travail pratique #1 - Medecin.cpp
+* Date: 31 janvier 2017
+* Auteur: Julien Bergeron (1829496) et Loic Bellemare-Alford (1846135)
+*******************************************/
+
 #include "Specialite.h"
 #include <iostream>
 #include <iomanip>
 #include "Const.h"
 
+
+/*
+Constructeur par défaut de la classe Specialiste
+*/
 Specialite::Specialite()
 {
 }
 
+
+/*
+Construteur avec les paramètres pour la classe Specialiste
+\param [in]		domaine		Indique le domaine
+\param [in]		niveau		Indique le niveau
+*/
 Specialite::Specialite(const std::string & domaine, unsigned int niveau): domaine_(domaine), niveau_(niveau)
 {
 }
 
+/*
+Méthode pour obtenir l'attribut
+\return domaine_
+*/
 std::string Specialite::obtenirDomaine() const
 {
 	return domaine_;
 }
 
+/*
+Méthode pour obtenir l'attribut
+\return niveau_
+*/
 unsigned int Specialite::obtenirNiveau() const
 {
 	return niveau_;
 }
 
+/*
+Méthode pour modifier l'attribut
+\param[in]	unDomaine	Indique un domaine
+*/
 void Specialite::modifierDomaine(const std::string & domaine)
 {
 	domaine_ = domaine;
 }
 
+/*
+Méthode pour modifier l'attribut
+\param[in]	unNiveau	Indique un niveau
+*/
 void Specialite::modifierNiveau(unsigned int niveau)
 {
 	niveau_ = niveau;
 }
 
-void information() const // A MODIFIER... (si necessaire)
-{
-	cout << " | " << specialite.domaine_ << AFFICHER_ESPACE(espace_domaine - specialite.domaine_.size());
-	cout << " | " << AFFICHER_ESPACE(espace_niveau - std::to_string(specialite.niveau_).size()) << specialite.niveau_;
 
-	cout << AFFICHER_ESPACE(espace_niveau) << " | ";
+/*
+Affiche les informations sur la specialite
+*/
+void Specialite::information() const 
+{
+	
+	std::cout << " | " << domaine_  << AFFICHER_ESPACE(espace_domaine - domaine_.size());
+	std::cout << " | " << AFFICHER_ESPACE(espace_niveau - std::to_string(niveau_).size()) << niveau_;
+
+	std::cout << AFFICHER_ESPACE(espace_niveau) << " | ";
 }
+
